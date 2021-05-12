0168648ab5109a87a7a6f0284973365582cca0ea
ngocquektqd <ngocquektqd@gmail.com>
2021-05-12 22:59:16 +0700

    commit

--- app/src/main/java/com/lengocque/MainActivity.java
+++ app/src/main/java/com/lengocque/MainActivity.java
@@ -1,14 +1,74 @@
 package com.lengocque;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.lengocque.adapter.ItemAdapter;
+import com.lengocque.model.Item;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
 
+    RecyclerView recyclerViewMain;
+    TextView tvCnow,tvstatusNow;
+
+    List<Item> list=new ArrayList<>();
+    ItemAdapter adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getdata();
+        adapter =new ItemAdapter(this,list);
+        RecyclerView.LayoutManager layoutManager=new LinearLayoutManager(this,RecyclerView.HORIZONTAL,false);
+
+        recyclerViewMain=findViewById(R.id.rvMain);
+        recyclerViewMain.setLayoutManager(layoutManager);
+        recyclerViewMain.setAdapter(adapter);
+        tvCnow=findViewById(R.id.tvCnow);
+        tvstatusNow=findViewById(R.id.tvstatusNow);
+
+    }
+    public void getdata(){
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(ManagerApi.DOMAIN)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+        ManagerApi apimanager=retrofit.create(ManagerApi.class);
+        apimanager.getdata().enqueue(new Callback<List<Item>>() {
+            @Override
+            public void onResponse(Call<List<Item>> call, Response<List<Item>> response) {
+
+                if(response.body()!=null){
+                    list=response.body();
+                    adapter.reLoadData(list);
+                    response.code();
+                    tvstatusNow.setText(list.get(0).getIconPhrase());
+                    int Cnow= (int) list.get(0).getTemperature().getValue();
+                    tvCnow.setText((Cnow+"°"));
+
+                }
+            }
+
+            @Override
+            public void onFailure(Call<List<Item>> call, Throwable t) {
+
+
+                Toast.makeText(MainActivity.this,"Fail",Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/lengocque/adapter/ItemAdapter.java
@@ -0,0 +1,98 @@
+package com.lengocque.adapter;
+
+import android.app.Activity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.lengocque.R;
+import com.lengocque.model.Item;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+
+public class ItemAdapter extends RecyclerView.Adapter {
+    private Activity activity;
+    private List<Item> itemDataList;
+
+    public ItemAdapter(Activity activity, List<Item> itemDataList) {
+        this.activity = activity;
+        this.itemDataList = itemDataList;
+    }
+
+
+    public void reLoadData(List<Item> list){
+        this.itemDataList=list;
+        this.notifyDataSetChanged();
+    }
+
+
+    @NonNull
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View dataViewHolder=activity.getLayoutInflater().inflate(R.layout.item_layout,parent,false);
+        ItemDataHolder holder=new ItemDataHolder(dataViewHolder);
+
+        return holder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+
+
+        ItemDataHolder dataHolder= (ItemDataHolder) holder;
+        Item data=itemDataList.get(position);
+        dataHolder.tvTime.setText(convertTime(data.getDateTime()));
+
+        dataHolder.tvC.setText(String.valueOf(data.getTemperature().getValue()));
+        int weatherIcon=data.getWeatherIcon();
+        if (weatherIcon<10){
+            String urlicon="https://developer.accuweather.com/sites/default/files/0"+weatherIcon+"-s.png";
+            Glide.with(activity).load(urlicon).into(dataHolder.ivIcon);
+        }
+        if(weatherIcon>=10){
+            String urlicon="https://developer.accuweather.com/sites/default/files/"+weatherIcon+"-s.png";
+            Glide.with(activity).load(urlicon).into(dataHolder.ivIcon);
+        }
+
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return itemDataList.size();
+    }
+    public  class ItemDataHolder extends RecyclerView.ViewHolder {
+
+    TextView tvTime,tvC;
+    ImageView ivIcon;
+    public ItemDataHolder(@NonNull View itemView) {
+        super(itemView);
+        tvTime=itemView.findViewById(R.id.tvTime);
+        tvC=itemView.findViewById(R.id.tvC);
+        ivIcon=itemView.findViewById(R.id.ivIcon);
+
+    }
+}
+
+    public String convertTime(String inputTime) {
+        SimpleDateFormat inFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss");
+        Date date = null;
+        try {
+            date = inFormat.parse(inputTime);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        SimpleDateFormat outFormat = new SimpleDateFormat("ha");
+        String goal = outFormat.format(date);
+        return goal;
+    }
+
+}
